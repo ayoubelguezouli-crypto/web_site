7abb5b1212ae923b008f1fcca74bf36c3e5a4bbf
ayoubelguezouli-crypto <ayoubelguezouli@gmail.com>
2026-02-03 21:26:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function scrollTo(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
-}
+// Contact button opens email
+document.getElementById("contactBtn").onclick = () => {
+  window.location.href = "mailto:ayoubelguezouli@gmail.com";
+};
+
+// Social icons open your accounts
+document.getElementById("facebook").onclick = () => {
+  window.open("https://www.facebook.com/minecraft.dolibo?mibextid=ZbWKwL", "_blank");
+};
+
+document.getElementById("snapchat").onclick = () => {
+  window.open("https://www.snapchat.com/add/ayoubelguezouli?share_id=xiTGXlS7oYo&locale=en-GB", "_blank");
+};
